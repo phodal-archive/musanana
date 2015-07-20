256fe42301d06469497c976fa72283fa37ded534
Fengda HUANG <h@phodal.com>
2015-07-20 08:38:49 +0800

    init project

---
+++ test/index.js
@@ -0,0 +1,9 @@
+'use strict';
+import assert from 'assert';
+import qian from '../lib';
+
+describe('qian', function () {
+  it('should have unit test!', function () {
+    assert(true, 'we expected this package author to add actual unit tests.');
+  });
+});
