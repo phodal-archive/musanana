c3100725d61318af67a23b8495e834fb56a94d69
Fengda HUANG <h@phodal.com>
2015-07-21 09:13:29 +0800

    add ionic init project

--- test/index.js
+++ test/index.js
@@ -8,6 +8,6 @@ describe('musanana', function () {
   it('should have unit test!', function () {
     var lex = fs.readFileSync(path.normalize("test/spec.minion"), 'utf8');
     var musanana = new Musanana(lex);
-    assert(musanana.check, true);
+    assert(musanana.createSuccessful, true);
   });
 });
