730304d96b0c9b959e67b89d67c2fc2313e9ba4e
Fengda HUANG <h@phodal.com>
2015-07-22 09:12:12 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -8,6 +8,7 @@ describe('musanana', function () {
   it('should have unit test!', function () {
     var lex = fs.readFileSync(path.normalize("test/spec.minion"), 'utf8');
     var musanana = new Musanana(lex);
+    musanana.run();
     assert(musanana.createSuccessful, true);
   });
 });
